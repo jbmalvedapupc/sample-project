293212a62d93f24984d3eda6f6dc8f5fce8e5cf5
Jed Malveda <jbmalveda.pupc@gmail.com>
2020-11-17 12:36:52 +0800

    Added view files

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'FileController@index')->name('files.index');
+Route::get('/files/create', 'FileController@create')->name('files.create');
